fd4d0610fcbc40106dd31239f5ade227f8406e28
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2025-02-04 02:01:39 +0200

    added native wind for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/components/**/*.{js,jsx,ts,tsx}",
+    "./app/screens/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
